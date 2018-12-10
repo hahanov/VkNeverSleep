84a43000e78f6c07285d0be0ec4f723e28fee662
hahanov <khahanovka@gmail.com>
2018-12-11 00:20:54 +0300

    Create ineversleep.js

---
+++ ineversleep.js
@@ -0,0 +1,21 @@
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+} function sendMsg(txt) { sleep(50000).then(() => {
+   var el = document.querySelector('div.im_editable.im-chat-input--text._im_text');
+	el.innerText = txt;
+	el.dispatchEvent(new Event('focus'));
+	el.dispatchEvent(new Event('keydown'));
+	setTimeout( function(){
+		var btn = document.querySelector('button.im-send-btn.im-chat-input--send._im_send.im-send-btn_send');
+		console.log("btn:", btn);
+		if( btn) {
+			btn.dispatchEvent(new Event('click'));
+		} else {
+			console.log("no button");
+		}		
+	}, 0); // Do something after the sleep!
+});
+	
+}
+
+sendMsg("I never sleep");
